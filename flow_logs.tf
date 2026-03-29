e42d6cdfec91d3c2c62598434d13432cce5742da
Pratik Das <pdas@Pratiks-MacBook-M4-Pro.local>
2026-03-29 01:40:30 -0500

    initial commit to the terraform aws vpc module

--- flow_logs.tf
+++ flow_logs.tf
@@ -26,7 +26,7 @@ resource "aws_kms_key" "flow_logs" {
         Sid    = "AllowCloudWatchLogs"
         Effect = "Allow"
         Principal = {
-          Service = "logs.${data.aws_region.current.name}.amazonaws.com"
+          Service = "logs.${data.aws_region.current.region}.amazonaws.com"
         }
         Action = [
           "kms:Encrypt",
@@ -38,7 +38,7 @@ resource "aws_kms_key" "flow_logs" {
         Resource = "*"
         Condition = {
           ArnLike = {
-            "kms:EncryptionContext:aws:logs:arn" = "arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:/aws/vpc/flow-logs/${var.name}"
+            "kms:EncryptionContext:aws:logs:arn" = "arn:aws:logs:${data.aws_region.current.region}:${data.aws_caller_identity.current.account_id}:log-group:/aws/vpc/flow-logs/${var.name}"
           }
         }
       }
